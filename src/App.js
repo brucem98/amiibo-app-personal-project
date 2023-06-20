207519b4b940b501d94d1c770a8eeda543029fa5
Bruce Milliken <84877032+brucem98@users.noreply.github.com>
2023-06-20 23:51:07 +0100

    Import react-router-dom in App.js and some css edits

--- src/App.js
+++ src/App.js
@@ -1,14 +1,24 @@
 import React from 'react';
+import {  
+  RouterProvider, 
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route 
+} from 'react-router-dom';
+
+import { Root } from './app/components/Root';
 import { AmiibosList } from './features/amiibos/AmiibosList';
 
+const router = createBrowserRouter(createRoutesFromElements(
+  <Route path="/" element={ <Root/> }>
+       <Route path="/" element={<AmiibosList/>}/>
+  </Route>
+)) 
+
 function App() {
-
-
   return (
-    <div className="App">
-      <AmiibosList />
-    </div>
-  );
+    <RouterProvider router={ router } />
+  )
 }
 
 export default App;
