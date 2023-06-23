b63d393ee4da42db1764e69bf11c926b6ba89d69
Bruce Milliken <84877032+brucem98@users.noreply.github.com>
2023-06-23 13:09:57 +0100

    add Link and NavLink to Header and AmiibosList components

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,15 @@
 import React from 'react'
 import logo from '../resources/images/Amiibo_logo.png'
+
+import { NavLink } from 'react-router-dom'
+
 export const Header = () => {
     return (
       <header className='header-container'>
         <img src={logo} alt='Amiibo Logo'/>
         <nav>
-          <a href=''>Favorites</a>
-          <a href=''>Go Back</a>
+          <NavLink to=''>Favorites</NavLink>
+          <NavLink to=''>Go Back</NavLink>
         </nav>
       </header> 
    )
--- src/features/amiibos/AmiibosList.js
+++ src/features/amiibos/AmiibosList.js
@@ -1,24 +1,32 @@
 import React from 'react'
 import { useSelector } from 'react-redux'
 
+import { Link } from 'react-router-dom'
+
 export const AmiibosList = () => {
     const amiibos = useSelector(state => state.amiibos)
 
     const renderedAmiibos = amiibos.map(amiibo => (
         <article key={amiibo.id} className="amiibo-tile">
+            <Link to= 
+                {amiibo.name.concat(' amiibo ', amiibo.gameSeries).replaceAll(' ', '-').toLowerCase()}
+                className='single-amiibo-link'
+            >
             <h3>{amiibo.name}</h3>
             <img src={amiibo.image} alt={`${amiibo.name} Amiibo`}/>
             <div>
                 <p>Game Series: {amiibo.gameSeries}</p>
-                <p>Amiibo Series: {amiibo.gameSeries}</p>
+                <p>Amiibo Series: {amiibo.amiiboSeries}</p>
             </div>
+            </Link>
         </article>
+        
     ))
     console.log(amiibos)
 
     return (
         <>
-            <section className="amiibo-tile-container">
+            <section className="amiibo-tiles-container">
                 {renderedAmiibos}
             </section>
         </>
