4c966840f809a1ed3389f32aa74f1df08bd07b91
Bruce Milliken <84877032+brucem98@users.noreply.github.com>
2023-06-25 22:51:31 +0100

    few changes in SingleAmiiboPage component

--- src/features/amiibos/SingleAmiiboPage.js
+++ src/features/amiibos/SingleAmiiboPage.js
@@ -7,8 +7,19 @@ export const SingleAmiiboPage = () => {
     const amiibo = useSelector(state => state.amiibos).find(amiibo => amiibo.id === id)
    
     return (
-        <article>
-            <h1>{amiibo.name}</h1>
+        <article className='single-amiibo-container'>
+            <img src={amiibo.image} alt='amiibo image'/>
+            <section>
+                <h2>{amiibo.name}</h2>  
+                <h3>{amiibo.gameSeries}</h3>
+                <h3>{amiibo.amiiboSeries}</h3>
+                <ul>
+                    <li>{amiibo.release.au}</li>
+                    <li>{amiibo.release.eu}</li>
+                    <li>{amiibo.release.jp}</li>
+                    <li>{amiibo.release.na}</li>
+                </ul>
+            </section>
         </article>
     )
 }
