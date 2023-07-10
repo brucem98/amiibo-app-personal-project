7814366b1b2a01ac92f9556e2504a6e3447b1e5f
Bruce Milliken <84877032+brucem98@users.noreply.github.com>
2023-07-10 19:44:43 +0100

    styled SingleAmiiboPage component

--- src/features/amiibos/SingleAmiiboPage.js
+++ src/features/amiibos/SingleAmiiboPage.js
@@ -1,6 +1,10 @@
 import React from 'react'
 import { useSelector } from 'react-redux';
-import { Link, useParams } from 'react-router-dom'
+import { Link, useParams } from 'react-router-dom';
+import au from '../../resources/images/au-flag-icon.png';
+import eu from '../../resources/images/eu-flag-icon.png';
+import jp from '../../resources/images/jp-flag-icon.png';
+import na from '../../resources/images/na-flag-icon.png';
 
 export const SingleAmiiboPage = () => {
     const { id } = useParams();
@@ -8,17 +12,20 @@ export const SingleAmiiboPage = () => {
    
     return (
         <article className='single-amiibo-container'>
-            <img src={amiibo.image} alt='amiibo image'/>
-            <section>
-                <h2>{amiibo.name}</h2>  
-                <h3>{amiibo.gameSeries}</h3>
-                <h3>{amiibo.amiiboSeries}</h3>
-                <ul>
-                    <li>{amiibo.release.au}</li>
-                    <li>{amiibo.release.eu}</li>
-                    <li>{amiibo.release.jp}</li>
-                    <li>{amiibo.release.na}</li>
-                </ul>
+            <img src={amiibo.image} alt='amiibo' className='single-amiibo-image'/>
+            <section className='single-amiibo-info'>
+                <div className='single-info-1'>
+                    <h2>{amiibo.name}</h2>  
+                    <h3>Game Series: <span className='info-style'>{amiibo.gameSeries}</span> </h3>
+                    <h3>Amiibo Series: <span className='info-style'>{amiibo.amiiboSeries}</span> </h3>
+                </div>
+                <h3 className='releases-title'>Release Date By Region:</h3>
+                <ul className='single-info-2 info-style'>
+                    <li><img src={au} alt='Australia Icon'/>{amiibo.release.au}</li>
+                    <li><img src={eu} alt='EU Icon'/>{amiibo.release.eu}</li>
+                    <li><img src={jp} alt='Japan Icon'/>{amiibo.release.jp}</li>
+                    <li><img src={na} alt='NA Icon'/>{amiibo.release.na}</li>
+                </ul>  
             </section>
         </article>
     )
