eac3b184585c56557fa13c334bbd388f4b240a38
Bruce Milliken <84877032+brucem98@users.noreply.github.com>
2023-07-18 13:02:49 +0100

    Create Pagination.js component

---
+++ src/components/Pagination.js
@@ -0,0 +1,24 @@
+import React from 'react'
+
+export const Pagination = ({ currentPage, totalPages, onPageChange }) => {
+    const pageNumbers = [];
+    
+    for (let i = 1; i <= totalPages; i++) {
+        pageNumbers.push(i);
+    }
+
+    return (
+        <ul className='pagination'>
+            {pageNumbers.map(number => (
+                <li
+                    key={number}
+                    className={number === currentPage ? 'active' : ''}
+                    onClick={() => onPageChange(number)}
+                >
+                    {number}
+                </li>
+            ))}
+        </ul>
+    )
+}
+
