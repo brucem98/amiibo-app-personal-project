f0f8c6bae0b35e0594622d6e49f1c274c5da1ae0
Bruce Milliken <84877032+brucem98@users.noreply.github.com>
2023-07-18 17:22:51 +0100

    finalize NavLink and go back functionality in header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,17 +1,31 @@
 import React from 'react'
 import logo from '../resources/images/Amiibo_logo.png'
-
-import { NavLink } from 'react-router-dom'
+import { Link, NavLink, useNavigate } from 'react-router-dom'
 
 export const Header = () => {
-    return (
-      <header className='header-container header-background'>
-        <img src={logo} alt='Amiibo Logo'/>
-        <nav>
-          <NavLink to='/amiibos/favorites'>Favorites</NavLink>
-          <NavLink to=''>Go Back</NavLink>
-        </nav>
-      </header> 
-   )
+  const navigate = useNavigate();
+
+  return (
+    <header className='header-container header-background'>
+      <Link to= {'/'}>
+        <img src={logo} alt='Amiibo Logo' className='inactiveNavLink'/>
+      </Link>
+      <nav>
+        <ul>
+          <li>
+            <NavLink 
+              to='/amiibos/favorites'
+              className={({ isActive }) => isActive ? 'activeNavLink' : 'inactiveNavLink'}
+            >
+              Favorites
+            </NavLink>
+          </li>
+          <li>
+            <NavLink onClick={() => navigate(-1)} className='inactiveNavLink'>Go Back</NavLink>
+          </li>
+        </ul>
+      </nav>
+    </header> 
+  )
 }
 
