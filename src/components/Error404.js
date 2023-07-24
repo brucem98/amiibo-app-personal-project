f16f0f97211bf8721144cca4ac7ae3e0d8e1fb48
Bruce Milliken <84877032+brucem98@users.noreply.github.com>
2023-07-24 11:23:02 +0100

    create conditional nav link rendering for Error404 page

--- src/components/Error404.js
+++ src/components/Error404.js
@@ -1,7 +1,19 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import { toggleNavLinks } from './navLinks/navLinksSlice';
 import marioerror from '../resources/images/nes-mario-dying.png';
 
 export const Error404 = () => {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(toggleNavLinks())
+    return () => {
+      dispatch(toggleNavLinks())
+    }
+  }, [dispatch])
+
+
   return (
     <section className='error-page-container'>
       <h1>ERROR 404</h1>
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,30 +1,41 @@
-import React from 'react'
-import logo from '../resources/images/Amiibo_logo.png'
-import { Link, NavLink, useNavigate } from 'react-router-dom'
+import React from 'react';
+import logo from '../resources/images/Amiibo_logo.png';
+import { Link, NavLink, useNavigate } from 'react-router-dom';
+import { useSelector, useDispatch } from 'react-redux';
+import { toggleNavLinks } from './navLinks/navLinksSlice';
+
 
 export const Header = () => {
   const navigate = useNavigate();
+  const showNavLinks = useSelector(state => state.navLinks.showNavLinks);
+  const dispatch = useDispatch();
+
+  const handleToggleNavLinks = () => {
+    dispatch(toggleNavLinks())
+  };
 
   return (
     <header className='header-container header-background'>
       <Link to= {'/'}>
         <img src={logo} alt='Amiibo Logo' className='inactiveNavLink'/>
       </Link>
-      <nav>
-        <ul>
-          <li>
-            <NavLink 
-              to='/amiibos/favorites'
-              className={({ isActive }) => isActive ? 'activeNavLink' : 'inactiveNavLink'}
-            >
-              Favorites
-            </NavLink>
-          </li>
-          <li>
-            <NavLink onClick={() => navigate(-1)} className='inactiveNavLink'>Go Back</NavLink>
-          </li>
-        </ul>
-      </nav>
+      {showNavLinks && (
+        <nav>
+          <ul>
+            <li>
+              <NavLink 
+                to='/amiibos/favorites'
+                className={({ isActive }) => isActive ? 'activeNavLink' : 'inactiveNavLink'}
+              >
+                Favorites
+              </NavLink>
+            </li>
+            <li>
+              <NavLink onClick={() => navigate(-1)} className='inactiveNavLink'>Go Back</NavLink>
+            </li>
+          </ul>
+        </nav>
+      )}
     </header> 
   )
 }
